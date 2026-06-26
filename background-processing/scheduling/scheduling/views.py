e4317cdc75f776c2a6f89918f735c460377865e6
Leandro Rodrigues <leandromartinsr10@hotmail.com>
2026-06-26 14:22:08 -0300

    feat(view): Endpoints para demonstração de agendamentos
    Descrição
    - Endpoint para execução em thread dedicada.
    - Endpoint para execução assíncrona com `asyncio.create_task()`.
    - Endpoint para simulação de agendamento persistido.
    - Endpoint para consulta de agendamentos persistidos.
    - Evidencia a limitação atual do Django REST Framework em cenários assíncronos.

---
+++ background-processing/scheduling/scheduling/views.py
@@ -0,0 +1,28 @@
+import asyncio
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from scheduling.scheduler import execucao_thread, agendamento_persistido, execucao_async_ASGI, AGENDAMENTOS
+
+
+@api_view(["GET"])
+def get_agendamentos(request):
+    return Response(status=200, data={"data": AGENDAMENTOS})
+
+
+@api_view(["POST"])
+def thread(request):
+    result = execucao_thread()
+    return Response(status=201, data={"message": result.message})
+
+
+@api_view(["POST"])
+def persistencia(request):
+    result = agendamento_persistido()
+    return Response({"message": result.message})
+
+
+@api_view(["POST"])
+async def async_scheduling(request):
+    await execucao_async_ASGI()
+    return Response(status=202, data={"message": "Estoura erro porque o DRF ainda não suporta totalmente async."})
